b079b0a3005504416f68b904cb18095bb059981f
MinaMaher0 <minamaher30697@gmail.com>
2020-05-11 05:23:00 +0200

    solve error

--- tests/unit/services/ToDoService.spec.js
+++ tests/unit/services/ToDoService.spec.js
@@ -58,6 +58,6 @@ describe('ToDoService', () => {
     const item2 = deletedItem()
     LocalStorageService.setItem(STORAGE_KEY, [item, item2])
     ToDoService.removeItem(item.id)
-    expect(ToDoService.fetchAllWithDeleted()).toEqual("error")
+    expect(ToDoService.fetchAllWithDeleted()).toEqual([item2])
   })
 })
